1272811fba4948ab7e5542514c5eabdbb09b48eb
Jason <jasonsu894@gmail.com>
2022-08-01 02:55:49 -0400

    Added new activity.

---
+++ app/src/main/java/com/example/cscb07_project/ViewUserEventsActivity.java
@@ -0,0 +1,14 @@
+package com.example.cscb07_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewUserEventsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_user_events);
+    }
+}
